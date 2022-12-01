6b562a7bbc28f10c459402e33f01557c606f3231
Andrey Zakharenkov <zahaand@gmail.com>
2022-12-01 17:03:05 +0300

    add getters and setters

--- src/main/java/ru/zahaand/astontrainee/model/Employee.java
+++ src/main/java/ru/zahaand/astontrainee/model/Employee.java
@@ -17,4 +17,20 @@ public class Employee {
     @Size(min = 2, max = 100, message = "Имя должно быть длиной от 2 до 100 символов")
     @Column(name = "full_name")
     private String fullName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
