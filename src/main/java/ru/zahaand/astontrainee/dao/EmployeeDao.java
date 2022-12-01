efd62ef18e3abf5c8b981d681009065b59b22879
Andrey Zakharenkov <zahaand@gmail.com>
2022-12-01 14:47:35 +0300

    add dao Employee

---
+++ src/main/java/ru/zahaand/astontrainee/dao/EmployeeDao.java
@@ -0,0 +1,12 @@
+package ru.zahaand.astontrainee.dao;
+
+import ru.zahaand.astontrainee.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeDao {
+    void createOrUpdate(Employee employee);
+    Employee read(int id);
+    void delete(int id);
+    List<Employee> getAllEmployees();
+}
---
+++ src/main/java/ru/zahaand/astontrainee/dao/EmployeeDaoImp.java
@@ -0,0 +1,42 @@
+package ru.zahaand.astontrainee.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.zahaand.astontrainee.model.Employee;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDaoImp implements EmployeeDao {
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public EmployeeDaoImp(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void createOrUpdate(Employee employee) {
+        sessionFactory.getCurrentSession().saveOrUpdate(employee);
+    }
+
+    @Override
+    public Employee read(int id) {
+        return sessionFactory.getCurrentSession().get(Employee.class, id);
+    }
+
+    @Override
+    public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        session.remove(session.get(Employee.class, id));
+    }
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        Session session = sessionFactory.getCurrentSession();
+        return session.createQuery(
+                "select Employee  from Employee", Employee.class).getResultList();
+    }
+}
