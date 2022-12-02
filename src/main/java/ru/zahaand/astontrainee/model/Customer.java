3b45c038e323863685366736a2aeae6776bab5c1
Andrey Zakharenkov <zahaand@gmail.com>
2022-12-02 11:00:02 +0300

    add models

--- src/main/java/ru/zahaand/astontrainee/model/Customer.java
+++ src/main/java/ru/zahaand/astontrainee/model/Customer.java
@@ -6,20 +6,19 @@ import jakarta.validation.constraints.Size;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "employee")
-public class Employee {
+@Table(name = "customer")
+public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private int id;
 
-    @NotEmpty(message = "Имя не должно быть пустым")
+    @NotEmpty(message = "Имя заказчика не должно быть пустым")
     @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов")
-    @Column(name = "full_name")
-    private String fullName;
+    @Column(name = "customer_name")
+    private String customerName;
 
-    @ManyToOne
-    @JoinColumn(name = "project_id", referencedColumnName = "id")
+    @OneToOne(mappedBy = "customer")
     private Project project;
 
     public int getId() {
@@ -30,12 +29,12 @@ public class Employee {
         this.id = id;
     }
 
-    public String getFullName() {
-        return fullName;
+    public String getCustomerName() {
+        return customerName;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
     }
 
     public Project getProject() {
---
+++ src/main/java/ru/zahaand/astontrainee/model/Project.java
@@ -0,0 +1,58 @@
+package ru.zahaand.astontrainee.model;
+
+import jakarta.validation.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "project")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NotEmpty(message = "Название проекта не должно быть пустым")
+    @Column(name = "project_name")
+    private String projectName;
+
+    @OneToMany(mappedBy = "project")
+    private List<Employee> employees;
+
+    @OneToOne
+    @JoinColumn(name = "customer_id", referencedColumnName = "id")
+    private Customer customer;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+}
